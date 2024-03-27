8faf5e1591589a6eaedce5b511df7986de8151bb
flexxdarker <ksenzhakmax@gmail.com>
2024-03-27 20:40:30 +0200

    edited something

--- src/app/services/films.ts
+++ src/app/services/films.ts
@@ -2,13 +2,16 @@ export interface FilmModel {
     id: number;
     name: string;
     categoryId: number;
+    catogoryName: string;
+    companyId: number;
+    companyName: string;
     imageUrl: string;
     year: number;
 }
 
-export interface FilmResponseModel {
-    films: FilmModel[];
-    limit: number;
-    skip: number;
-    total: number;
-}
+// export interface FilmResponseModel {
+//     films: FilmModel[];
+//     limit: number;
+//     skip: number;
+//     total: number;
+// }
